cb5e5ac77a623fab4091972325d75afc1159c06d
artur.filippovskii <arturfilippovskij@gmail.com>
2026-05-16 14:33:37 +0300

    ci: add ci setup for project

--- jest.config.mjs
+++ jest.config.mjs
@@ -7,6 +7,14 @@ export default {
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'json-summary', 'lcov'],
+  coverageThreshold: {
+    global: {
+      statements: 90,
+      branches: 90,
+      functions: 90,
+      lines: 90,
+    },
+  },
   collectCoverageFrom: [
     'src/**/*.js',
     'bin/**/*.js',
